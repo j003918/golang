7a58b0421de2809212f1f1f3ce682b456a6e93b2
j003918 <j003918@foxmail.com>
2017-07-06 13:46:43 +0800

    add yaml

--- project/src/test/main.go
+++ project/src/test/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	//	"fmt"
-	//"net/url"
+	"fmt"
 	"freenovel"
 )
 
 func main() {
-	//freenovel.NovelSearch("官家")
-	freenovel.NovelDownload("http://www.88dushu.com/xiaoshuo/22/22322/")
+	freenovel.WebsiteList()
+
+	bu := ""
+	for {
+		fmt.Print("Please input novel url: ")
+		fmt.Scanln(&bu)
+		freenovel.NovelDownload(bu)
+	}
+
+	//freenovel.NovelSearch("弄潮")
+	//freenovel.NovelDownload("http://www.shoujikanshu.org/xiaoshuo/index_21793.html")
 	//freenovel.NovelDownload("http://www.xxbiquge.com/65_65338/")
 	//freenovel.NovelDownload("http://www.23us.com/html/67/67612/")
 	//freenovel.NovelDownload("http://www.88dushu.com/xiaoshuo/71/71885/")
 	//freenovel.NovelDownload("http://www.qu.la/book/4336/")
 	//freenovel.NovelDownload("http://www.biqudao.com/bqge29882/")
-
 }
