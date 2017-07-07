12e357420537adf79c3a2d463c0e1f795e7886f6
j003918 <j003918@foxmail.com>
2017-07-07 12:32:46 +0800

    xxxx

--- project/src/test/main.go
+++ project/src/test/main.go
@@ -14,12 +14,4 @@ func main() {
 		fmt.Scanln(&bu)
 		freenovel.NovelDownload(bu)
 	}
-
-	//freenovel.NovelSearch("弄潮")
-	//freenovel.NovelDownload("http://www.shoujikanshu.org/xiaoshuo/index_21793.html")
-	//freenovel.NovelDownload("http://www.xxbiquge.com/65_65338/")
-	//freenovel.NovelDownload("http://www.23us.com/html/67/67612/")
-	//freenovel.NovelDownload("http://www.88dushu.com/xiaoshuo/71/71885/")
-	//freenovel.NovelDownload("http://www.qu.la/book/4336/")
-	//freenovel.NovelDownload("http://www.biqudao.com/bqge29882/")
 }
